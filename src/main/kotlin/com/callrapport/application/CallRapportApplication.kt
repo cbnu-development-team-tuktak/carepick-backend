1386b0490824d9432d660511a7674cc4413b0059
Lunarian0928 <thanks092834@gmail.com>
2025-03-16 14:34:37 +0900

    메인 함수 관련 주서 처리 완료

--- src/main/kotlin/com/callrapport/application/CallRapportApplication.kt
+++ src/main/kotlin/com/callrapport/application/CallRapportApplication.kt
@@ -1,10 +1,13 @@
 package com.callrapport
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
+
+// Spring Boot 애플리케이션 관련
+import org.springframework.boot.autoconfigure.SpringBootApplication // Spring Boot의 자동 설정을 활성화하는 어노테이션
+import org.springframework.boot.runApplication // 애플리케이션을 실행하는 함수
 
 @SpringBootApplication
 class CallRapportApplication
 
 fun main(args: Array<String>) {
+	// Spring Boot 애플리케이션 실행
 	runApplication<CallRapportApplication>(*args)
 }
