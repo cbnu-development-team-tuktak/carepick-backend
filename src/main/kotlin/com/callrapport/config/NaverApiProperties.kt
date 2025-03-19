76241f7577013cda72a8f81424df7068674bc5c2
Lunarian0928 <thanks092834@gmail.com>
2025-03-19 22:40:26 +0900

    네이버 api 연동을 위한 config 파일 추가 완료

---
+++ src/main/kotlin/com/callrapport/config/NaverApiProperties.kt
@@ -0,0 +1,9 @@
+package com.callrapport.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "naver")
+data class NaverApiProperties(
+    var clientId: String = "zb92fols6g",
+    var secret: String = "D22P4B37g3yJpLYDqjrLMfqlqadJby3fLkipdncy" 
+)
