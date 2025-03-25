9a6aa3118731165bb416036239f1448ba73d6853
Lunarian0928 <thanks092834@gmail.com>
2025-03-25 20:08:10 +0900

    <Hospital DTO 수정 완료> + 좌표 값을 Point 값이 아닌 소수로 반환하도록 convert 함수 구현

--- src/main/kotlin/com/callrapport/dto/HospitalDetailsResponse.kt
+++ src/main/kotlin/com/callrapport/dto/HospitalDetailsResponse.kt
@@ -4,6 +4,8 @@ import com.callrapport.model.hospital.Hospital
 import com.callrapport.model.hospital.HospitalAdditionalInfo
 import com.callrapport.model.hospital.HospitalImage
 
+import org.locationtech.jts.geom.Point
+
 data class HospitalDetailsResponse(
     val id: String, // 병원 ID
     val name: String, // 병원명
@@ -15,7 +17,8 @@ data class HospitalDetailsResponse(
     val specialties: List<String>?, // 병원의 진료과 목록 (선택적 필드, NULL 허용)
     val doctors: List<String>?, // 병원에 소속된 의사 목록 (선택적 필드, NULL 허용)
     val images: List<ImageResponse>?, // 병원과 연결된 이미지 정보 추가
-    val additionalInfo: HospitalAdditionalInfo? // 병원 추가 정보 (선택적 필드, NULL 허용)
+    val additionalInfo: HospitalAdditionalInfo?, // 병원 추가 정보 (선택적 필드, NULL 허용)
+    val location: LatLng? // 병원 위치 정보
 ) {
     companion object {
         // Hospital 엔티티를 HospitalDetailsResponse DTO로 변환
@@ -44,7 +47,9 @@ data class HospitalDetailsResponse(
                     .takeIf { it.isNotEmpty() } // 비어 있지 않은 경우에만  수행
                     ?.map { ImageResponse.from(it) }, // ImageResponse로 변환
 
-                additionalInfo = hospital.additionalInfo // 병원 부가 정보
+                additionalInfo = hospital.additionalInfo, // 병원 부가 정보
+
+                location = hospital.location.toLatLng()
             )
         }
     }
@@ -70,5 +75,17 @@ data class ImageResponse(
                 alt = hospitalImage.image.alt
             )
         }
+    }
+}
+
+// 병원 좌표 정보를 담을 클래스
+data class LatLng(
+    val latitude: Double, // 위도
+    val longitude: Double // 경도
+)
+
+fun Point?.toLatLng(): LatLng? {
+    return this?.let {
+        LatLng(latitude = it.y, longitude = it.x)
     }
 }
